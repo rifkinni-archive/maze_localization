2bdc352efc7e662e2c71b3eecd9d88f1c3de7fd1
rifkinni <rifkinni@gmail.com>
2015-12-14 11:55:39 -0500

    paul's help

--- scripts/maze_robot.py
+++ scripts/maze_robot.py
@@ -12,17 +12,17 @@ from geometry_msgs.msg import Twist, Vector3
 from maze_solver import MazeSolver
 from tf.transformations import euler_from_quaternion
 
-class MazeRobot(object):
+class MazeNavigator(object):
     """ Main controller for the robot maze solver """
     def __init__(self):
         """ Main controller """
-        rospy.init_node('maze_robot')
+        rospy.init_node('maze_navigator')
 
         self.odom = None
         self.prevOdom = None
-        self.projected = [0]*361
+        self.scan = []
+        self.projected = []
 
-        self.projector = MazeProjector()
         self.solver = MazeSolver()
 
         self.currentI = 0 #index to keep track of our instruction
@@ -45,89 +45,160 @@ class MazeRobot(object):
         """ updates on new scan data
             data: LaserScan data
         """
-        self.projector.scan = data.ranges
+        if not self.scan:
+            self.laserScan = data
+        self.laserScan.header = data.header
+        self.scan = data.ranges
 
 
     def callbackOdom(self, data):
         """ updates on new odom data
             data: Odometry data
         """
-        self.projector.odom = data.pose
         self.odom = self.convert_pose_to_xy_and_theta(data.pose)
         if not self.prevOdom: #first reading
             self.prevOdom = self.odom #no change
 
     def performInstruction(self):
-        """ acts as a run loop to perform instructions
-
+        """ sets twist and updates maze scan
+            based on current instruction and odometry reading
         """
 
-        if not self.odom:
+        if not self.odom or not self.prevOdom:
             return
 
+        c = .5 #proportional control constant
         instruction = self.solver.instructions[self.currentI]
-        c = .5
-        diffPos, diffAng = self.calcDifference(self.odom, self.prevOdom, instruction)
+        diffPos, diffAng = self.calcDifference(instruction) #difference in position, difference in angle
 
-        if abs(diffAng)%(2*math.pi) < .05:
+        if abs(diffAng)%(2*math.pi) < .05: #turned to correct orientation
             self.turn = False
         
-        if abs(diffPos) < .05:
+        if abs(diffPos) < .05: #moved forward successfully to next node
+            self.currentI += 1 #increment instruction
+            self.turn = True 
+            self.prevOdom = self.odom #update odometry
+            wall = self.getWalls()#instruction[1])
+            self.projected = self.projectMaze(wall) #update laser scan 
+            # self.laserScan.ranges = tuple(self.projectMaze(wall, instruction[1])) #update laser scan 
 
-            self.currentI += 1
-            self.turn = True
-            self.prevOdom = self.odom
 
-            wall = self.getWalls(instruction[1])
-            # currentNode = self.solver.path[self.currentI]
-            # neighbors = self.solver.getNeighbors(currentNode)
-
-            # get projected maze
-            self.laserScan.ranges = tuple(self.projectMaze(wall))
-
-        if self.turn:
+        if self.turn: #set angular velocity
             self.twist.angular.z = c * diffAng
             self.twist.linear.x = 0
-        else:
+            self.rotateWalls(self.odom[2])
+
+        else: #set linear velocity
             self.twist.linear.x = c *diffPos
             self.twist.angular.z = 0
 
 
-    def calcDifference(self, current, previous, instruction):
+    def angle_normalize(self, z):
+        """ convenience function to map an angle to the range [-pi,pi] """
+        return math.atan2(math.sin(z), math.cos(z))
+
+    def angle_diff(self, a, b):
+        """ Calculates the difference between angle a and angle b (both should be in radians)
+            the difference is always based on the closest rotation from angle a to angle b
+            examples:
+                angle_diff(.1,.2) -> -.1
+                angle_diff(.1, 2*math.pi - .1) -> .2
+                angle_diff(.1, .2+2*math.pi) -> -.1
+        """
+        a = self.angle_normalize(a)
+        b = self.angle_normalize(b)
+        d1 = a-b
+        d2 = 2*math.pi - math.fabs(d1)
+        if d1 > 0:
+            d2 *= -1.0
+        if math.fabs(d1) < math.fabs(d2):
+            return d1
+        else:
+            return d2
+
+    def calcDifference(self, instruction):
         """ calculate the difference in position and orientation between current and previous odometry
-            current: current odometry
-            previous: previous odometry
             returns tuple of form (difference in position, difference in orientation)
         """
-        distance = .3
+        distance = .3 #distance between nodes
+        # currAng = current[2]%(2*math.pi)
+        # prevAng = previous[2]%(2*math.pi)
+        diffPos = distance - math.sqrt((self.odom[0] - self.prevOdom[0])**2 + (self.odom[1] - self.prevOdom[1])**2)
+        diffAng = instruction[0] - self.angle_diff(self.odom[2],self.prevOdom[2])
+        # if self.turn:
+        #     print "angle difference", diffAng
+        #     print "odom current", currAng, "odom prev", prevAng
+        #     print "odom difference", current[2] - previous[2]
+        #     print "-----"
+        return diffPos, diffAng
 
-        currAng = current[2]%(2*math.pi)
-        prevAng = previous[2]%(2*math.pi)
-
-        if current and previous:
-            diffPos = distance - math.sqrt((current[0] - previous[0])**2 + (current[1] - previous[1])**2)
-            diffAng = instruction[0] - (current[2] - previous[2])
-            # if self.turn:
-            #     print "angle difference", diffAng
-            #     print "odom current", currAng, "odom prev", prevAng
-            #     print "odom difference", current[2] - previous[2]
-            #     print "-----"
-            return diffPos, diffAng
-        else:
-            return 0, 0
-    
-    def getWalls(self, orientation):
+    def getWalls(self):#, orientation):
         """ get a representation of maze walls the robot can understand
             currentNode: coordinates of current node
             orientation: current orientation of the robot
+            returns: list of length 4 with binary entries
+                True is a path
+                False is a wall
         """
         currentNode = self.solver.path[self.currentI]
         neighbors = self.solver.getNeighbors(currentNode)
-        walls = [None]*4
+        walls = [None]*4 #default is all walls
         for nextNode in neighbors:
             nextOrient = self.solver.getNextOrientation(currentNode, nextNode)
-            walls[nextOrient] = 1
-        return walls[orientation:] + walls[:orientation]
+            walls[nextOrient] = 1 #update list with True where paths exist
+        return walls #absolute position
+
+
+    def projectMaze(self, wall):
+        """ get 'laser scan' ranges for the virtual maze based on surrounding walls
+            wall: output from getWalls
+            returns a list of length 359 with maze scan data to be published
+        """
+        wallDistance = .1
+        maxDistance = .3
+        projected = [0]*361
+
+        for i in range(1, 360):
+            if i <= 45 or i > 315:
+                if not wall[0]:
+                   projected[i] = wallDistance / math.cos(i*math.pi / 180)
+                else:
+                    c = 1.0 if i <= 45 else -1.0
+                    distance = wallDistance / math.sin(i*math.pi / 180) *c
+                    projected[i] = 0 if distance > maxDistance else distance
+
+            elif i <= 135:
+                if not wall[3]:
+                    projected[i] = wallDistance / math.sin(i*math.pi / 180)
+                else:
+                    c = 1.0 if i <= 90 else -1.0
+                    distance = wallDistance /  math.cos(i*math.pi / 180) * c
+                    projected[i] = 0 if distance > maxDistance else distance
+
+            elif i <= 225:
+                if not wall[2]:
+                    projected[i] = wallDistance / -math.cos(i*math.pi / 180)
+                else:
+                    c = 1.0 if i <= 180 else -1.0
+                    distance = wallDistance / math.sin(i*math.pi / 180) *c
+                    projected[i] = 0 if distance > maxDistance else distance
+
+            elif i <= 315:
+                if not wall[1]:
+                    projected[i] = wallDistance / -math.sin( i*math.pi / 180)
+                else:
+                    c = -1.0 if i <= 270 else 1.0
+                    distance = wallDistance / math.cos(i*math.pi / 180)*c
+                    projected[i] = 0 if distance > maxDistance else distance
+        return projected
+
+    def rotateWalls(self, orientation):
+        if self.projected:
+            orientation = int(orientation * 180/math.pi)
+            rotatedProjection = self.projected[orientation:] + self.projected[:orientation]
+            self.laserScan.ranges = tuple(rotatedProjection)
+            self.pubScan.publish(self.laserScan) #publish maze scan
+
 
 
     def convert_pose_to_xy_and_theta(self, pose):
@@ -142,66 +213,27 @@ class MazeRobot(object):
         return pose.pose.position.x, pose.pose.position.y, angles[2]
             
     def run(self):
-        """ Our main 5Hz run loop """
-
+        """ Our main 5Hz run loop
+        """
         r = rospy.Rate(5)
         while not rospy.is_shutdown():
-            if self.currentI < len(self.solver.instructions):
+            if self.currentI < len(self.solver.instructions): #still have instructions to perform
                 self.performInstruction()
-                self.pubScan.publish(self.laserScan)
-                print "laser scan published"
+                # self.pubScan.publish(self.laserScan) #publish maze scan
                 self.pubVel.publish(self.twist)
                 r.sleep()
 
-            else:
-                self.twist.linear.x = 0
+            else: 
+                self.twist.linear.x = 0 #stop the robot
                 self.twist.angular.z = 0
                 print "done traversing the maze"
                 self.pubVel.publish(self.twist)
-                break 
+                break #exit
                 
 
-    def projectMaze(self, wall):
-        """ set projected to the virtual maze """
-        wallDistance = 1
-        maxDistance = 5
 
-        for i in range(1, 360):
-            if i <= 45 or i > 315:
-                if not wall[0]:
-                    self.projected[i] = wallDistance / math.cos(i*math.pi / 180)
-                else:
-                    c = 1.0 if i <= 45 else -1.0
-                    distance = wallDistance / math.sin(i*math.pi / 180) *c
-                    self.projected[i] = 0 if distance > maxDistance else distance
-
-            elif i <= 135:
-                if not wall[1]:
-                    self.projected[i] = wallDistance / math.sin(i*math.pi / 180)
-                else:
-                    c = 1.0 if i <= 90 else -1.0
-                    distance = wallDistance /  math.cos(i*math.pi / 180) * c
-                    self.projected[i] = 0 if distance > maxDistance else distance
-
-            elif i <= 225:
-                if not wall[2]:
-                    self.projected[i] = wallDistance / -math.cos(i*math.pi / 180)
-                else:
-                    c = 1.0 if i <= 180 else -1.0
-                    distance = wallDistance / math.sin(i*math.pi / 180) *c
-                    self.projected[i] = 0 if distance > maxDistance else distance
-
-            elif i <= 315:
-                if not wall[3]:
-                    self.projected[i] = wallDistance / -math.sin( i*math.pi / 180)
-                else:
-                    c = -1.0 if i <= 270 else 1.0
-                    distance = wallDistance / math.cos(i*math.pi / 180)*c
-                    self.projected[i] = 0 if distance > maxDistance else distance
-        return self.projected
 
 
 if __name__ == '__main__':
-    node = MazeRobot()
-
+    node = MazeNavigator()
     node.run()
